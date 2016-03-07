5c1074050ff5f44e4cf34778b713ce14bf9cbcca
Maxim Gordeev <maksim.gordeew2012@yandex.ru>
2016-03-08 01:26:51 +0300

    Пробный пуск

---
+++ IZ10/IZ10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IZ10.MvcApplication" Language="C#" %>
---
+++ IZ10/IZ10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IZ10.Startup))]
+namespace IZ10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
